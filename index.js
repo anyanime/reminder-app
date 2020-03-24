720233d0a72533eb7126038c678d7d391a0d27c1
Anyanime <bensonanyanime@gmail.com>
2020-03-24 17:22:04 +0100

    modified files

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 "use strict"
 const fetch = require('node-fetch')
-const salute = 'The Management AWLO Ikeja, Lagos.'
 const phone = '+2349022631479'
-const smsMessage = `Good day, 
-${salute} This is to inform you that the light unit in the office will finish in the next 24hours. Kindly send money to benson to load the light unit. Thanks In anticipation of your response. -AWLOLECTRIC BOT.`
-const smsFrom = 'AWLOLECTRIC BOT'
-const URL = ('https://www.bulksms.com/api/v1/sms/create')
+const smsMessage = 'Good day, This is to inform you that the light unit in the office will finish in the next 24hours. Kindly send money to benson to load the light unit. Thanks In anticipation of your response. -AWLOLECTRIC BOT.'
+const smsFrom = 'AWLO BOT'
+const token = 'IF0WLu86pvylyphsaYMDuYHGRFAbJBBA9h7pF5Is7As5Rq9U1qOLxkGtoSHI'
 
-sendSMS = () => {  
-fetch(`${URL}?api_token=KKbt94pIsFsY2iRuSqgRZGYm3xwVstMaXBaH0tCMLZhj9uYvanCdtgejFUPi&from=${smsFrom}&to=${phone}&body=${smsMessage}&dnd=2`) 
-.then(res => res.json())
+const sendSMS = () => {  
+fetch(
+    `https://www.bulksmsnigeria.com/api/v1/sms/create?api_token=${token}&from=${smsFrom}&to=${phone}&body=${smsMessage}&dnd=2`
+    ) 
+.then(res => res.text())
 .then(body => console.log(body))
 .catch(err => console.log(err))
 }

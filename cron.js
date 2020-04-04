f8850fcbff84b8ffc071a3b9d893c6dbfd1cfefd
Anyanime <bensonanyanime@gmail.com>
2020-04-04 19:34:38 +0100

    edited files

--- cron.js
+++ cron.js
@@ -2,9 +2,9 @@ const mainscript = require("./index.js")
 const CronJob = require('cron').CronJob
 
 console.log('Before job instantiation')
-const job = new CronJob('0/2 * * * * *', function () {
+const job = new CronJob('0/5 * * * * *', function () {
     // mainscript.sendSMS()
-    mainscript.sendMail().catch(console.error)
+    mainscript.sendMail
     console.log('Message sent')
 })
 console.log('After job instantiation')
